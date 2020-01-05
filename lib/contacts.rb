a817b8100f66937f9708ba0850c118ee5182f361
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 23:29:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,16 @@ require 'pry'
   #   }
   # }
   
+ def remove_strawberry(contacts)
+   
+ 
+ 
+  
+  
+  
+  
+  
+  
 # def remove_strawberry(contacts)
 #   contacts.each do |person, contact_details_hash|
 #     if person == "Freddy Mercury"
@@ -25,20 +35,4 @@ require 'pry'
 #     end
 #   end
 # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
